c8b639a6b1566bfd1cecadadd599bcd868c142dd
Lindokuhle2420 <lindokuhlekubheka99@gmail.com>
2026-08-27 18:44:52 +0200

    Scope Practise Done

---
+++ day3-module-6/Scope_practise.js
@@ -0,0 +1,14 @@
+// Global Scope
+let a ='Awes';
+
+function scopetest() {
+    let a = "wola"
+    console.log(a);
+
+   
+    
+}
+ let b = "Greetinggs"
+console.log(b);
+console.log(a);
+scopetest();

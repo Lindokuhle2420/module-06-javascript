e7cd6104ef5f16fc45d05a97ef3cf9a1c81d0c51
Lindokuhle2420 <lindokuhlekubheka99@gmail.com>
2026-08-27 16:32:43 +0200

    Iterations Practise

---
+++ day3-module-6/practice_revise.js
@@ -0,0 +1,26 @@
+// tenary operator
+let age = 25
+
+//nodlet age = price >=20 ? "You are too Young": "You are old enough";
+
+console.log ("I can be allowed because I am" + " "+ age);
+
+// Arrays
+let fruits = ["Kiwi","Strawberry","Orange"];
+console.log(fruits);
+
+//add an element
+fruits[0] = "Avo"; 
+
+fruits.unshift("Avocado");
+
+console.log(fruits)
+let numofFruits = fruits.length;
+console.log("the number of fruits we have is"+" "+ numofFruits)
+
+let index = fruits.indexOf("Avo");
+console.log (index);
+
+for(let i =0; i <fruits.length;i++){
+    console.log(fruits[i]);
+}
